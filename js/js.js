931e80b4003fcba400ff37a6d1475f1ad91c89df
Ahmed Adel <89941547+AhmedKazanova@users.noreply.github.com>
2023-02-07 21:52:06 +0200

    Added image to Hello World

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@
 
 let goToTop = document.getElementById("goToTop")
 let header = document.getElementById("header")
+let headingType = document.getElementById("demoType")
 goToTop.onclick = function(){window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}
 
 window.addEventListener("scroll", function() {
@@ -15,3 +16,19 @@ window.addEventListener("scroll", function() {
         header.classList.remove("active")
     }
 });
+
+
+var i = 0;
+var txt =  `شـركـة الـرائـدة الكـبـرى تُـعـد الرائدة في مجال برمجة وتصميم المواقع وتطبيقات الجوال والمتاجر الإلكترونية , نسعى دائما أن نكون الخيار الأول لأفكارك الإبداعية والمتميزة وتحويلها الى حقيقة بأعلى جودة وفي أسرع وقت للمساهمة في انجاح مشروعك`
+var speed = 70; 
+
+function typeWriter() {
+  if (i < txt.length) {
+    if(headingType != null || headingType != undefined){
+      headingType.innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+}
+typeWriter()
